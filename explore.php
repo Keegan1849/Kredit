43c5c785098145d23ae413d124b417d927cea41d
Williams <23WilliamsP@mydacc.org>
2023-03-02 11:56:33 -0500

    YO

--- explore.php
+++ explore.php
@@ -4,6 +4,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <title>Home</title>
         <!-- Bootstrap icons-->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
@@ -12,9 +15,9 @@
         <link rel="stylesheet" href="scripts/style2.css">
         <link rel="stylesheet" href="scripts/explore.css">
     </head>
-    <body>
+    <body class="bg">
         <!-- Navigation-->
-     <nav class="navbar navbar-expand-lg navbar-dark bg-dark stickyNav">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark stickyNav">
             <div class="container px-4 px-lg-5">
                 <a class="navbar-brand" href="explore.php">Kredit</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
@@ -25,6 +28,7 @@
                         <li class="nav-item"><a class="nav-link" href="favorites.php">Favorites</a></li>
                         
                     </ul>
+                    
                 </div>
             </div>
         </nav>
@@ -42,4 +46,5 @@
             <span id="card-total">
         </div>
     </body>
+    
 </html>
